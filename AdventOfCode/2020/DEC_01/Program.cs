79f0eb674aeba5e5d322ab9172557f8846ebb95c
Nathaniel Collier <drxs3v3n@gmail.com>
2020-12-29 09:31:27 -0600

    Working out a way to allways get a specific folder
    Need to be able to have a data folder at the root, that is seperate from the program code.  Working out a way to find the root of the repo, so we can have the data folder in a known location

--- AdventOfCode/2020/DEC_01/Program.cs
+++ AdventOfCode/2020/DEC_01/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace DEC_01
 {
@@ -6,7 +7,27 @@ namespace DEC_01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string rootPath = getRootDirectory();
+
+            Console.WriteLine($"Root Path: [{rootPath}]");
+        }
+
+        static string getRootDirectory()
+        {
+            string exePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string dirPath = System.IO.Path.GetDirectoryName(exePath);
+
+            // find parent that is key
+            string key = "repl_csharp_sln";
+            var pieces = dirPath.Split('/', '\\').Select(p => p.ToLower());
+
+            var rootIndex = pieces.ToList().FindIndex(p => string.Equals(p, key, StringComparison.OrdinalIgnoreCase));
+
+            var rootPathList = pieces.Take(rootIndex + 1);
+
+            string rootPath = string.Join(System.IO.Path.DirectorySeparatorChar, rootPathList);
+
+            return rootPath;
         }
     }
 }
