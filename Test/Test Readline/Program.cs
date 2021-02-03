053ea49942ffdd3c6083c452da3c4c4ea4cc3591
Nathaniel Collier <drxs3v3n@gmail.com>
2021-02-02 18:24:35 -0600

    Add simple test to practice with console readline in vscode

---
+++ Test/Test Readline/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Test_Readline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string searchText = "";
+
+            do{
+                Console.WriteLine("Enter some text (q=quit()): ");
+                searchText = Console.ReadLine();
+
+                Console.WriteLine($"You entered: {searchText}");
+            }while( !string.Equals(searchText, "q", StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
