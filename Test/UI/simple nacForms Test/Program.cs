9164741d433bc72bf278f482df84522db1412efa
Nathaniel Collier <drxs3v3n@gmail.com>
2021-02-02 18:32:09 -0600

    Successfully tested a simple gui app

---
+++ Test/UI/simple nacForms Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using dotnetCoreAvaloniaNCForms;
+
+namespace simple_nacForms_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var f = Avalonia.AppBuilder.Configure<dotnetCoreAvaloniaNCForms.App>()
+                                    .NewForm();
+
+            f.Text("Hello World!");
+            f.Display();
+        }
+    }
+}
