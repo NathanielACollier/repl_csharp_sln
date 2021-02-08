bf7f17da8ef3ffa2359f5e0eaf8b7786135af5c1
Nathaniel Collier <drxs3v3n@gmail.com>
2021-02-07 18:49:41 -0600

    Working on figuring out how Avalonia Datagrid works

---
+++ Test/UI/Avalonia Datagrid Testing1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using dotnetCoreAvaloniaNCForms;
+
+namespace Avalonia_Datagrid_Testing1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var f = Avalonia.AppBuilder.Configure<dotnetCoreAvaloniaNCForms.App>()
+                .NewForm();
+
+            f.Text("Hello World!");
+
+            var dg = new Avalonia.Controls.DataGrid();
+            dg.AutoGenerateColumns = true;
+
+            var people = new ObservableCollection<models.Person>
+            {
+                new models.Person
+                {
+                    First = "Nathaniel",
+                    Last = "Collier"
+                },
+                new models.Person
+                {
+                    First = "Heather",
+                    Last = "Collier"
+                }
+            };
+            f._Extend_AddBinding<ObservableCollection<models.Person>>("items", dg, Avalonia.Controls.DataGrid.ItemsProperty, false);
+            f._Extend_AddRowToHost(dg, rowAutoHeight: false);
+
+            f.Text("Below Datagrid");
+            
+            f.Display();
+        }
+    }
+}
---
+++ Test/UI/Avalonia Datagrid Testing1/models/Person.cs
@@ -0,0 +1,9 @@
+namespace Avalonia_Datagrid_Testing1.models
+{
+    public class Person
+    {
+        public string First { get; set; }
+        public string Last { get; set; }
+        
+    }
+}
