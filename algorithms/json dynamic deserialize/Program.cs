0dbe7a454d45802943d0de254cc588be8512fbe9
Nathaniel Collier <nathaniel.a.collier@gmail.com>
2021-02-27 13:07:19 -0600

    Experimenting with json

---
+++ algorithms/json dynamic deserialize/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace json_dynamic_deserialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // practicing stuff found at this place: https://stackoverflow.com/questions/3142495/deserialize-json-into-c-sharp-dynamic-object
+            dynamic stuff = JsonConvert.DeserializeObject("{ 'Name': 'Jon Smith', 'Address': { 'City': 'New York', 'State': 'NY' }, 'Age': 42 }");
+
+            string name = stuff.Name;
+            string address = stuff.Address.City;
+        }
+    }
+}
