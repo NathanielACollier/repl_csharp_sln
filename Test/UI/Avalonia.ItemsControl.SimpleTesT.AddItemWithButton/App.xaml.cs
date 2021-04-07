c88708cbdb2cf047cf10ebac21ab9d75290d125f
Nathaniel Collier <drxs3v3n@gmail.com>
2021-04-06 19:11:13 -0500

    Started a simple example
    This is so we can show the problem with Avalonia on macos, where you can't add an item to a list for some reason...

---
+++ Test/UI/Avalonia.ItemsControl.SimpleTesT.AddItemWithButton/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Markup.Xaml;
+
+namespace Avalonia.ItemsControl.SimpleTesT.AddItemWithButton
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ Test/UI/Avalonia.ItemsControl.SimpleTesT.AddItemWithButton/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Avalonia.Controls;
+
+namespace Avalonia.ItemsControl.SimpleTesT.AddItemWithButton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = getApp();
+
+            var win = new Avalonia.Controls.Window();
+            win.Show();
+            app.Run(win);
+        }
+
+        static Avalonia.Application getApp()
+        {
+            var appBuilder = Avalonia.AppBuilder.Configure<App>();
+            
+            var builder = appBuilder
+                //.LogToDebug(Avalonia.Logging.LogEventLevel.Verbose)
+                .UsePlatformDetect()
+                .SetupWithoutStarting();
+
+            return builder.Instance;
+        }
+        
+    }
+}
