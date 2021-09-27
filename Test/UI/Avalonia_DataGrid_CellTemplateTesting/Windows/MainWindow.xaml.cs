348fabbf5bb36f224014cb6154da14b38dcff222
Nathaniel Collier <drxs3v3n@gmail.com>
2021-09-27 15:29:50 -0500

    Got an example of the problem

--- Test/UI/Avalonia_DataGrid_CellTemplateTesting/Windows/MainWindow.xaml.cs
+++ Test/UI/Avalonia_DataGrid_CellTemplateTesting/Windows/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Templates;
 using Avalonia.Markup.Xaml;
 
 namespace Avalonia_DataGrid_CellTemplateTesting.Windows
@@ -17,6 +18,26 @@ namespace Avalonia_DataGrid_CellTemplateTesting.Windows
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+            addExtraColumns();
+        }
+
+        private void addExtraColumns()
+        {
+            var dg = this.FindControl<DataGrid>("dg1");
+            
+            var col = new Avalonia.Controls.DataGridTemplateColumn();
+            col.Header = "First Name (Code)";
+            col.CellTemplate = new FuncDataTemplate<object>((itemModel, namescope) =>
+            {
+                var p = itemModel as model.Person;
+
+                return new Avalonia.Controls.TextBlock
+                {
+                    Text = p.Firstname
+                };
+            });
+            
+            dg.Columns.Add(col);
         }
     }
 }
