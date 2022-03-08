7139ad3de27ed5c5e059d08f341f11b893ffce47
Nathaniel A Collier <drxs3v3n@gmail.com>
2022-03-08 17:45:08 -0600

    Got the window setup

--- Test/PDF/PDF Viewer Via Image/Program.cs
+++ Test/PDF/PDF Viewer Via Image/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using nac.Forms;
+
+var form = Avalonia.AppBuilder.Configure<nac.Forms.App>()
+    .NewForm();
+    
+form.Display();
