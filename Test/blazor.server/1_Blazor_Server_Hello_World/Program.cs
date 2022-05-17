0a346ec10a9adb391c69af5fe00dcd0a2964b5a3
Nathaniel A Collier <drxs3v3n@gmail.com>
2022-05-17 08:35:20 -0500

    Pulling stuff back in to get this to run

--- Test/blazor.server/1_Blazor_Server_Hello_World/Program.cs
+++ Test/blazor.server/1_Blazor_Server_Hello_World/Program.cs
@@ -12,5 +12,6 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");  // This is critical for it to loadup Pages/_Host.cshtml which is the starting point.  If you don't have this line you'll just see a not found error page
 
 app.Run();
