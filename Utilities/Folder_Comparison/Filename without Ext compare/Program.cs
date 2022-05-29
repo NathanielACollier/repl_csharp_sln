790fea2f986e367abe8e50f86e2809c44b70f976
Nathaniel A Collier <drxs3v3n@gmail.com>
2022-05-29 14:20:49 -0500

    Starting some programs to do some basic folder comparrisons

---
+++ Utilities/Folder_Comparison/Filename without Ext compare/Program.cs
@@ -0,0 +1,27 @@
+﻿
+string sourceFolderPath = @"C:\Users\drxs3\Downloads\roms\snes";
+
+string destinationFolderPath = @"C:\Users\drxs3\OneDrive\ROM\Super Nintendo";
+
+
+var sourceFiles = System.IO.Directory.EnumerateFiles(path: sourceFolderPath).Select(fi=> new {
+    Name = System.IO.Path.GetFileNameWithoutExtension(fi),
+    Path = fi
+});
+
+var destinationFiles = System.IO.Directory.EnumerateFiles(path: destinationFolderPath).Select(fi=> new {
+    Name = System.IO.Path.GetFileNameWithoutExtension(fi),
+    Path= fi
+});
+
+
+// find files in sourcefile where the name isn't in the destination folder right???
+
+var sourceNotInDestination = from sourceFI in sourceFiles
+                            let destMatch = destinationFiles.FirstOrDefault(destFI=> string.Equals(sourceFI.Name, destFI.Name))
+                            where destMatch == null
+                            select sourceFI;
+
+foreach( var fi in sourceNotInDestination){
+    Console.WriteLine($"Moving in {fi.Path}");
+}
---
+++ Utilities/Folder_Comparison/Rename every file in folder/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+var folderPath = @"C:\Users\drxs3\Downloads\roms\Game Boy Advance";
+
+var files = System.IO.Directory.EnumerateFiles(path: folderPath)
+    .Select(filePath =>
+    {
+        var result = (Path: filePath, isRename: false, newName: "");
+        var matches = System.Text.RegularExpressions.Regex.Match(filePath, @"(?<seq>\d+\s-\s)(?<name>.*)");
+
+        if (matches.Success)
+        {
+            result.isRename = true;
+            result.newName = matches.Groups["name"].Value;
+        }
+
+        return result;
+    })
+    .ToList();
+
+foreach (var file in files.Where(_f => _f.isRename))
+{
+    Console.WriteLine($"Renaming ||\t\t{System.IO.Path.GetFileName(file.Path)}\t||\t\t to || {file.newName} ||");
+
+    string destFilename = System.IO.Path.Combine(folderPath, file.newName);
+
+    if (System.IO.File.Exists(destFilename))
+    {
+        Console.WriteLine($"Deleting\t\t{destFilename}");
+        System.IO.File.Delete(destFilename);
+    }
+    
+    System.IO.File.Move(sourceFileName: file.Path,
+        destFileName: destFilename
+        );
+}
