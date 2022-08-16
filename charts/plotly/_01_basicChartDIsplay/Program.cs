8e6f1e648bfdaa3dd76b5495b8db9bb14293c795
Nathaniel A Collier <drxs3v3n@gmail.com>
2022-08-16 10:58:00 -0500

    Implemented some of the building a plotly chart but running into problems

--- charts/plotly/_01_basicChartDIsplay/Program.cs
+++ charts/plotly/_01_basicChartDIsplay/Program.cs
@@ -1,9 +1,27 @@
 ﻿
 
 using nac.Forms;
+using Plotly.NET.CSharp;
+using Plotly.NET.ImageExport;
 
 var form = Avalonia.AppBuilder.Configure<nac.Forms.App>()
     .NewForm();
     
 form.Text("Chart Display")
-    .Display();
+    .Display(onDisplay: (__f) =>
+    {
+        
+    });
+
+byte[] buildChart()
+{
+    var values = new[] { 30, 10, 50, 10 };
+    var labels = new[] { "Napster", "Jack", "Honey", "Mittens" };
+
+    var pie = Chart.Pie<int,string, string>(values: values,
+        Labels: labels,
+        Text: "A simple bar chart");
+
+    string imageBase64 = pie.ToBase64JPGString(Width: 1908, Height: 1024);
+    
+}
