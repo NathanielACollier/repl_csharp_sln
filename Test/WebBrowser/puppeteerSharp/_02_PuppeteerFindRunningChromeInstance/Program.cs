6c8dd25edd153b41b0bb8814de211f3481257ddc
Nathaniel Collier <drxs3v3n@gmail.com>
2022-08-20 12:15:34 -0500

    Start working out how to grab a running version of chrome into puppeteer

---
+++ Test/WebBrowser/puppeteerSharp/_02_PuppeteerFindRunningChromeInstance/Program.cs
@@ -0,0 +1,31 @@
+﻿using PuppeteerSharp;
+
+var browser = await getBrowser();
+
+Console.WriteLine("App finished");
+
+
+async Task<Browser> getBrowser()
+{
+    var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+    {
+        Headless = false,
+        ExecutablePath = findChrome(),
+        Args = new[] { "--app=http://localhost/null" }
+    });
+
+    return browser;
+}
+
+
+string findChrome()
+{
+    var chromeProcs = System.Diagnostics.Process.GetProcessesByName("chrome");
+
+    if (!chromeProcs.Any())
+    {
+        throw new Exception("Chrome must be running for this code to find it");
+    }
+
+    return chromeProcs.First().Modules[0].FileName;
+}
