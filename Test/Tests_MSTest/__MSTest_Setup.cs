d8b16c355aac1635d31a452707e5ce743eee4618
Nathaniel A Collier <drxs3v3n@gmail.com>
2022-08-31 07:27:42 -0500

    Add the global test assembly init incase we need it later

---
+++ Test/Tests_MSTest/__MSTest_Setup.cs
@@ -0,0 +1,22 @@
+namespace Tests_MSTest;
+
+[TestClass]
+public class __MSTest_Setup
+{
+
+    [AssemblyInitialize()]
+    public static void MyTestInitialize(TestContext testContext)
+    {
+        System.Diagnostics.Debug.WriteLine("Test Starting");
+        
+        // This runs one time on starting the tests
+        // Add logging, or setting up variables here
+    }
+
+    [AssemblyCleanup]
+    public static void TearDown()
+    {
+            
+            
+    }
+}
