6973972a0ea09ec7961e0efdc730afa6ec07cab5
Nathaniel Collier <nathaniel.a.collier@gmail.com>
2022-09-06 10:30:36 -0500

    Setup tabs

--- Utilities/WindowsOnly/WindowsClipboardManager/repos/MainWIndowRepo.cs
+++ Utilities/WindowsOnly/WindowsClipboardManager/repos/MainWIndowRepo.cs
@@ -16,7 +16,27 @@ public static class MainWindowRepo
 
         myForm.Title = "Windows Clipboard Manager";
 
+        myForm.Tabs(t =>
+        {
+            t.Header = "Text";
+            t.Populate = PopulateTabText;
+        }, t=>
+        {
+            t.Header = "Image";
+            t.Populate = PopulateTabImage;
+        });
+
         myForm.Display();
+    }
+
+    private static void PopulateTabImage(Form t)
+    {
+        t.Text("Image on clipboard will show up here");
+    }
+
+    private static void PopulateTabText(Form t)
+    {
+        t.Text("This will be where clipboard text shows up");
     }
 }
 
