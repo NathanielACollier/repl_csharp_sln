7adff841b9a09a5e4e1f31504052877e7a105bcc
Nathaniel Collier <drxs3v3n@gmail.com>
2022-09-06 14:54:42 -0500

    Added a way to deploy the application

---
+++ Utilities/WindowsOnly/WindowsClipboardManager/deploy-Release.ps1
@@ -0,0 +1,8 @@
+﻿
+$deployPath = "$($env:USERPROFILE)\programs\WindowsClipboardManager"
+$buildConfig = "Release"
+
+& dotnet @("publish","-c", $buildConfig, "-o", "`"$deployPath`"", "-p:PublishSingleFile=true",
+		"-p:RuntimeIdentifier=win-x64", "-p:SelfContained=false", "-p:ExcludeSymbolsFromSingleFile=true"
+ )
+
