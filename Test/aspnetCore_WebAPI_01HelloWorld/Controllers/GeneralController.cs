43c3a891bb6a9abac3bafd0c7202c01d96afa1d8
Nathaniel Collier <drxs3v3n@gmail.com>
2022-09-24 19:50:31 -0500

    Put in minimal that calls the webapi for hello world and displays it on the webpage

--- Test/aspnetCore_WebAPI_01HelloWorld/Controllers/GeneralController.cs
+++ Test/aspnetCore_WebAPI_01HelloWorld/Controllers/GeneralController.cs
@@ -1,8 +1,20 @@
 ﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
 namespace aspnetCore_WebAPI_01HelloWorld.Controllers;
 
+[ApiController]
+[Route("api/[controller]")]
 public class GeneralController : ControllerBase
 {
 
+
+    [HttpGet, Route("Hello")]
+    public ContentResult GetHello()
+    {
+        return Content("Hello World!", "application/text");
+    }
+    
+    
 }
 
--- Test/aspnetCore_WebAPI_01HelloWorld/Program.cs
+++ Test/aspnetCore_WebAPI_01HelloWorld/Program.cs
@@ -4,12 +4,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
 app.UseStaticFiles();
 
 app.UseRouting();
 
-app.MapFallbackToPage("/_Host");  // This is critical for it to loadup Pages/_Host.cshtml which is the starting point.  If you don't have this line you'll just see a not found error page
+app.MapControllers();
+app.MapRazorPages();
 
 app.Run();
