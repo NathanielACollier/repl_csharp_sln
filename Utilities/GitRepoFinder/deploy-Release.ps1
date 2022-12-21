9a9ef241805795669b656a150d39a03abf69f763
Nathaniel Collier <nathaniel.a.collier@gmail.com>
2022-12-21 08:36:17 -0600

    Working on figuring out a deploy method

---
+++ Utilities/GitRepoFinder/deploy-Release.ps1
@@ -0,0 +1,17 @@
+﻿
+$deployDir = [system.io.directory]::CreateDirectory("$($env:USERPROFILE)\programs\GitRepoFinder")
+Write-Host "Deploying: " $deployDir.FullName
+
+# clear existing folder
+remove-item ([system.io.path]::Combine($deployDir.FullName, "*")) -Recurse -Force
+
+$buildConfig = "Release"
+
+$projectFile = Get-ChildItem -Path $PSScriptRoot | where { $_.Extension -eq ".csproj"} | select -First 1
+
+Write-Host "Building: " $projectFile
+
+& dotnet @("publish", $projectFile,  "-c", $buildConfig, "-o", "`"" + $deployDir.FullName + "`"", "-p:PublishSingleFile=true",
+		"-p:RuntimeIdentifier=win-x64", "-p:SelfContained=false", "-p:ExcludeSymbolsFromSingleFile=true"
+ )
+
