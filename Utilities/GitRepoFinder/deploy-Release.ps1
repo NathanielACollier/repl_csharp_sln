86fcae19ef6a7fe40c5c57dbc96816639fcfe50b
Nathaniel Collier <nathaniel.a.collier@gmail.com>
2022-12-21 09:32:00 -0600

    More fixes trying to figure out why this isn't working

--- Utilities/GitRepoFinder/deploy-Release.ps1
+++ Utilities/GitRepoFinder/deploy-Release.ps1
@@ -5,18 +5,16 @@ Write-Host "Project Folder: " $projectFolderPath
 $projectFileInfo = Get-ChildItem -Path $projectFolderPath | where { $_.Extension -eq ".csproj"} | select -First 1
 
 $deployDir = [system.io.directory]::CreateDirectory(  [System.IO.Path]::Combine([system.environment]::GetFolderPath("userprofile"), "programs", [system.io.path]::GetFileNameWithoutExtension($projectFileInfo.Name) ))
-Write-Host "Deploying: " $deployDir.FullName
 
 # clear existing folder
 remove-item ([system.io.path]::Combine($deployDir.FullName, "*")) -Recurse -Force
 
 $buildConfig = "Release"
 
-
-Write-Host "Building: " $projectFileInfo.FullName
-
 $projectFilePath = "`"" + $projectFileInfo.FullName + "`""
+Write-Host "Building: " $projectFilePath
 $deployDirPath = "`"" + $deployDir.FullName + "`""
+Write-Host "Deploying: " $deployDirPath
 
 & dotnet @("publish", $projectFilePath,  "-c", $buildConfig, "-o", $deployDirPath, "-p:PublishSingleFile=true",
 		"-p:RuntimeIdentifier=win-x64", "-p:SelfContained=false", "-p:ExcludeSymbolsFromSingleFile=true"
