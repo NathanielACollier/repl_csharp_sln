8c8a064ee2ba6a806ca2174f41d0cd4cd6c6563a
Nathaniel Collier <nathaniel.a.collier@gmail.com>
2022-12-21 09:27:50 -0600

    A little closer

--- Utilities/GitRepoFinder/deploy-Release.ps1
+++ Utilities/GitRepoFinder/deploy-Release.ps1
@@ -1,8 +1,8 @@
 ﻿
-$myScriptFolder = [System.IO.Path]::GetDirectoryName( $MyInvocation.MyCommand.Path )
-Write-Host "Script Folder: " $myScriptFolder
+$projectFolderPath = [System.IO.Path]::GetDirectoryName( $MyInvocation.MyCommand.Path )
+Write-Host "Project Folder: " $projectFolderPath
 
-$projectFileInfo = Get-ChildItem -Path $myScriptFolder | where { $_.Extension -eq ".csproj"} | select -First 1
+$projectFileInfo = Get-ChildItem -Path $projectFolderPath | where { $_.Extension -eq ".csproj"} | select -First 1
 
 $deployDir = [system.io.directory]::CreateDirectory(  [System.IO.Path]::Combine([system.environment]::GetFolderPath("userprofile"), "programs", [system.io.path]::GetFileNameWithoutExtension($projectFileInfo.Name) ))
 Write-Host "Deploying: " $deployDir.FullName
@@ -15,7 +15,10 @@ $buildConfig = "Release"
 
 Write-Host "Building: " $projectFileInfo.FullName
 
-& dotnet @("publish", "`"" + $projectFileInfo.FullName + "`"",  "-c", $buildConfig, "-o", "`"" + $deployDir.FullName + "`"", "-p:PublishSingleFile=true",
+$projectFilePath = "`"" + $projectFileInfo.FullName + "`""
+$deployDirPath = "`"" + $deployDir.FullName + "`""
+
+& dotnet @("publish", $projectFilePath,  "-c", $buildConfig, "-o", $deployDirPath, "-p:PublishSingleFile=true",
 		"-p:RuntimeIdentifier=win-x64", "-p:SelfContained=false", "-p:ExcludeSymbolsFromSingleFile=true"
  )
 
