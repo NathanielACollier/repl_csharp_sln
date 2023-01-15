4a6029d61cbcfbf1b1de40043ad9e1bfa88cf967
Nathaniel Collier <drxs3v3n@gmail.com>
2023-01-15 10:49:34 -0600

    Add lots more notes, then change the output to the root directory using the commonUtilities class

--- database/sqlite/sqliteEFCoreTest/db/TestContext.cs
+++ database/sqlite/sqliteEFCoreTest/db/TestContext.cs
@@ -10,6 +10,19 @@ namespace sqliteEFCoreTest.db;
 Add-Migration Initial
 Update-Database
 ```
++ If you are not on windows and need to run the ef core migrations there is a dotnet tool
+    + see: https://stackoverflow.com/questions/45382536/how-to-enable-migrations-in-visual-studio-for-mac
++ Install the dotnet ef global tool
+```powershell
+dotnet tool install --global dotnet-ef
+```
++ Now you can run `dotnet ef` in the project folder and it will show you the standard help info
++ Then once that works right you can run these commands to setup the database
+```powershell
+dotnet ef migrations add initial
+dotnet ef database update
+```
++ Now when you change something you can add another migration like `dotnet ef migrations add v2`.  Then run the `database update`
  */
 
 public class TestContext : DbContext
@@ -18,7 +31,8 @@ public class TestContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlite("Data Source=TestDB.db;");
+        string dbFilePath = System.IO.Path.Combine(commonUtilitiesLib.Directory.getRootDirectory(), "TestDB.db");
+        optionsBuilder.UseSqlite($"Data Source={dbFilePath};");
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
