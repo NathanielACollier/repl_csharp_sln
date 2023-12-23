3656ecf6184b4ce4e14faf4ba56c595f5b58df6d
Nathaniel Collier <drxs3v3n@gmail.com>
2023-12-23 11:17:02 -0600

    Need settings to store the azure keys

---
+++ lib/commonUtilitiesLib/settings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace commonUtilitiesLib;
+
+public static class settings
+{
+
+    public static string Get(string key){
+        string userProfileFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+        string settingsFilePath = System.IO.Path.Combine(userProfileFolderPath, "settings.json");
+        string settingsJsonText = System.IO.File.ReadAllText(settingsFilePath);
+        var settingsModel = System.Text.Json.JsonSerializer.Deserialize<Dictionary<string,object>>(json: settingsJsonText);
+
+        return Convert.ToString(settingsModel[key]);
+    }
+}
---
+++ lib/testCommonUtilitiesLib/settingsTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace testCommonUtilitiesLib;
+
+[TestClass]
+public class settingsTest
+{
+
+    [TestMethod]
+    public void GetSetting(){
+        string val = commonUtilitiesLib.settings.Get("test");
+
+        Assert.IsTrue(!string.IsNullOrWhiteSpace(val), "Test setting was empty");
+        Assert.IsTrue(val.Length > 3, "Test setting wasn't as long as it should have been");
+    }
+}
