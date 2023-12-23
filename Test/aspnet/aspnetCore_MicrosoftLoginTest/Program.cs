261580560611474216711c33985af1c84bc49cfc
Nathaniel Collier <drxs3v3n@gmail.com>
2023-12-23 11:23:58 -0600

    Working out Microsoft Login

--- Test/aspnet/aspnetCore_MicrosoftLoginTest/Program.cs
+++ Test/aspnet/aspnetCore_MicrosoftLoginTest/Program.cs
@@ -1,4 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+
+services.AddAuthentication().AddMicrosoftAccount(microsoftOptions =>
+    {
+        microsoftOptions.ClientId = commonUtilitiesLib.settings.Get("TestAuthAzureAppID");
+        microsoftOptions.ClientSecret = commonUtilitiesLib.settings.Get("TestAuthAzureClientSecret");
+    });
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
