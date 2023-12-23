1845a6645bc659ed9bb9bb7d7fd8422d5a5da731
Nathaniel Collier <drxs3v3n@gmail.com>
2023-12-23 12:05:25 -0600

    Add documentation

--- Test/aspnet/aspnetCore_MicrosoftLoginTest/Program.cs
+++ Test/aspnet/aspnetCore_MicrosoftLoginTest/Program.cs
@@ -2,6 +2,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 var services = builder.Services;
 
+/*
+Trying to follow this documentation:
+https://learn.microsoft.com/en-us/aspnet/core/security/authentication/social/microsoft-logins?view=aspnetcore-8.0
+*/
+
 services.AddAuthentication().AddMicrosoftAccount(microsoftOptions =>
     {
         microsoftOptions.ClientId = commonUtilitiesLib.settings.Get("TestAuthAzureAppID");
