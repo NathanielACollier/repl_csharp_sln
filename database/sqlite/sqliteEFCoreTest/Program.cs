f69e306da87ee8eb499099c99c2676937096b8f2
Nathaniel Collier <drxs3v3n@gmail.com>
2023-12-23 09:51:06 -0600

    Update with a visualizer

--- database/sqlite/sqliteEFCoreTest/Program.cs
+++ database/sqlite/sqliteEFCoreTest/Program.cs
@@ -8,14 +8,18 @@ using (var conn = new db.TestContext())
         Message = "Test Entry: " + Guid.NewGuid().ToString("N")
     });
     conn.SaveChanges();
-    
-    Console.WriteLine($"Row count: {conn.Test.Count()}");
+
+    var f = nac.Forms.Form.NewForm();
+    f.Model["rowCount"] = conn.Test.Count();
+    f.Model["rows"] = conn.Test.ToList();
 
     var lastRow = conn.Test.OrderByDescending(i => i.Id)
         .First();
     
-    Console.WriteLine($@"Last row:
-ID:      {lastRow.Id}
-Date:    {lastRow.Date}
-Message: {lastRow.Message}");
+    f.HorizontalGroup(hg=> {
+        hg.Text($"Last row: ID: {lastRow.Id}; Date: {lastRow.Date}");
+    })
+    .Table<db.Tables.TestTable>(itemsModelFieldName: "rows");
+
+    f.Display();
 }
