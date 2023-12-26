fac6afe0e0583ff5bc4a41975ba26cb23e37f446
Nathaniel A Collier <drxs3v3n@gmail.com>
2023-12-26 11:03:31 -0600

    A little further, and we have to fix the AddDefaultIdentiy.  Then maybe this will work

--- Test/aspnet/aspnetCore_MicrosoftLoginTest/Program.cs
+++ Test/aspnet/aspnetCore_MicrosoftLoginTest/Program.cs
@@ -1,9 +1,20 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var services = builder.Services;
+
+/*
+Might be a way to store the user identity in memory
+-- https://stackoverflow.com/questions/60316005/how-to-use-identity-in-asp-net-core-3-1-without-ef-core
+-- look at this too
+- https://learn.microsoft.com/en-us/aspnet/core/security/authentication/identity-custom-storage-providers?view=aspnetcore-3.1
+*/
+
+services.AddDefaultIdentity<IdentityUser>(options =>
+                                       options.SignIn.RequireConfirmedAccount = true);
 
 /*
 Trying to follow this documentation:
@@ -18,6 +29,9 @@ services.AddAuthentication()
     });
 
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapGet("/", () => "Hello World!");
 
