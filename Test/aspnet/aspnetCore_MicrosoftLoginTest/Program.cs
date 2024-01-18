0ce4b9a66d390847fa42e0ebdf9fe72eb9958c54
Nathaniel A Collier <drxs3v3n@gmail.com>
2024-01-18 13:14:21 -0600

    Getting closer to authorization working

--- Test/aspnet/aspnetCore_MicrosoftLoginTest/Program.cs
+++ Test/aspnet/aspnetCore_MicrosoftLoginTest/Program.cs
@@ -1,10 +1,23 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using appDB=aspnetCore_MicrosoftLoginTest.appDB;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var services = builder.Services;
+
+/*
+Need to run these commands to setup the database
+```
+dotnet tool install --global dotnet-ef
+dotnet ef database update
+```
+*/
+services.AddDbContext<appDB.ApplicationDbContext>(options =>
+        options.UseSqlite(connectionString: "Filename=./appDb.sqlite")
+        );
 
 /*
 Might be a way to store the user identity in memory
@@ -13,7 +26,13 @@ Might be a way to store the user identity in memory
 - https://learn.microsoft.com/en-us/aspnet/core/security/authentication/identity-custom-storage-providers?view=aspnetcore-3.1
 */
 
-services.AddIdentityCore<IdentityUser>();
+services.AddIdentityCore<IdentityUser>(options => 
+    { options.SignIn.RequireConfirmedAccount = true;
+        options.SignIn.RequireConfirmedEmail = true;
+
+    })
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<appDB.ApplicationDbContext>();
 
 /*
 Trying to follow this documentation:
@@ -26,6 +45,8 @@ services.AddAuthentication()
         microsoftOptions.ClientId = commonUtilitiesLib.settings.Get("TestAuthAzureAppID");
         microsoftOptions.ClientSecret = commonUtilitiesLib.settings.Get("TestAuthAzureClientSecret");
     });
+
+services.AddAuthorization();
 
 var app = builder.Build();
 
---
+++ Test/aspnet/aspnetCore_MicrosoftLoginTest/appDB/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace aspnetCore_MicrosoftLoginTest.appDB;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
+
+
+}
