e111d385118ea4b4b8adf28e3bceb6c537a41101
Nathaniel Collier <drxs3v3n@gmail.com>
2024-02-24 15:12:58 -0600

    Build login url

--- Test/aspnet/aspnetCore_MicrosoftLoginWithoutLibraryHelp/repositories/MicrosoftLogin.cs
+++ Test/aspnet/aspnetCore_MicrosoftLoginWithoutLibraryHelp/repositories/MicrosoftLogin.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace aspnetCore_MicrosoftLoginWithoutLibraryHelp;
 
 public static class MicrosoftLogin
 {
+    private static string graphRootUrl = "https://graph.microsoft.com";
 
     private static (string appID, string clientSecret) readEntraSettings(){
         return (appID: commonUtilitiesLib.settings.Get("TestAuthAzureAppID"),
@@ -20,7 +22,16 @@ public static class MicrosoftLogin
         loginUrl.Path = $"{tenant}/oauth2/authorize";
 
         // add the params
-        loginUrl.AddQuery("client_id", entraSettings.appID);
+        loginUrl.AddQueryParametersFromDictionary(new Dictionary<string,string>{
+            {"client_id", entraSettings.appID},
+            {"response_type", "code"},
+            {"redirect_uri", redirectUrl},
+            {"response_mode", "query"},
+            {"resource", graphRootUrl},
+            {"state", "12345"}
+        });
+
+        return loginUrl.ToString();
     }
 
 }
