5faa1102eaf80ef574050e393358b766eadf7c86
Nathaniel Collier <drxs3v3n@gmail.com>
2024-02-24 14:54:27 -0600

    Working on forming the Microsoft Login

---
+++ Test/aspnet/aspnetCore_MicrosoftLoginWithoutLibraryHelp/repositories/MicrosoftLogin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace aspnetCore_MicrosoftLoginWithoutLibraryHelp;
+
+public static class MicrosoftLogin
+{
+
+    private static (string appID, string clientSecret) readEntraSettings(){
+        return (appID: commonUtilitiesLib.settings.Get("TestAuthAzureAppID"),
+        clientSecret: commonUtilitiesLib.settings.Get("TestAuthAzureClientSecret")
+        );
+    }
+
+    private static string FormMicrosoftLoginUrl(string redirectUrl){
+        string tenant = "common"; // your appID has to be multi tentant to use the common tenant
+        var entraSettings = readEntraSettings();
+
+        var loginUrl = new nac.utilities.Url("https://login.microsoftonline.com/");
+        
+        loginUrl.Path = $"{tenant}/oauth2/authorize";
+
+        // add the params
+        loginUrl.AddQuery("client_id", entraSettings.appID);
+    }
+
+}
