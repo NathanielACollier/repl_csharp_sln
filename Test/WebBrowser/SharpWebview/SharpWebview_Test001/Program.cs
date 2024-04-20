1281d6a6db6619c1223b383810f067c8bb1be5d3
Nathaniel A Collier <drxs3v3n@gmail.com>
2024-04-20 10:13:09 -0500

    Put in an example of showing a web view in linux + Test it out on other operating systems

---
+++ Test/WebBrowser/SharpWebview/SharpWebview_Test001/Program.cs
@@ -0,0 +1,20 @@
+﻿
+/*
+ ## Linux
+ + Need libwebkit2gtk-4.0-dev
+ ## ARCH
+ + Install webkit2 dev with
+ ```
+ sudo pacman -S webkit2gtk
+ ```
+ */
+
+using (var webView = new SharpWebview.Webview())
+{
+    webView
+        .SetTitle("Google")             
+        .SetSize(1024, 768, SharpWebview.WebviewHint.None)
+        .SetSize(800, 600, SharpWebview.WebviewHint.Min)
+        .Navigate(new SharpWebview.Content.UrlContent("https://google.com"))
+        .Run();
+}
