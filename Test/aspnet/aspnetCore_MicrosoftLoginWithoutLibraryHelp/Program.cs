8a45451ee0f5fd784e0e1e70a5b430b3272d3bae
Nathaniel Collier <drxs3v3n@gmail.com>
2024-07-08 19:04:30 -0500

    Try to put in HAR logging.  I don't think this will work right yet

--- Test/aspnet/aspnetCore_MicrosoftLoginWithoutLibraryHelp/Program.cs
+++ Test/aspnet/aspnetCore_MicrosoftLoginWithoutLibraryHelp/Program.cs
@@ -1,14 +1,15 @@
 ﻿
 using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
 using lib= aspnetCore_MicrosoftLoginWithoutLibraryHelp.lib;
 
+var log = new nac.Logging.Logger();
 // show logging
-nac.Logging.Logger.OnNewMessage += (_s, _args)=>{
-    Console.WriteLine(_args.ToString());
-};
+nac.Logging.Appenders.ColoredConsole.Setup();
+log.Info("Website starting...");
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,4 +29,16 @@ app.UseRouting();
 app.MapControllers();
 app.MapRazorPages();
 
+// har logging stuff
+var harEntries = new List<nac.http.logging.har.model.Entry>();
+nac.http.logging.har.LoggingHandler.isEnabled = true;
+nac.http.logging.har.LoggingHandler.onMessage += (_s, _args) =>
+{
+    harEntries.Add(_args);
+};
+
 app.Run();
+
+// when website is done save the har entries
+string harFileJSON = nac.http.logging.har.lib.utility.BuildHARFileJSON(harEntries);
+System.IO.File.WriteAllText("http.har", harFileJSON);
