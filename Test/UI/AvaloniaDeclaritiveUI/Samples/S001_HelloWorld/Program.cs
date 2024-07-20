9f339304a21019e70edfd1dc3fbd7e2f7a8c831c
Nathaniel Collier <drxs3v3n@gmail.com>
2024-07-20 17:07:36 -0500

    Start a place to try using the declaritve UI in Avalonia

---
+++ Test/UI/AvaloniaDeclaritiveUI/Samples/S001_HelloWorld/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+/*
+Keep going with this
+https://sourcegraph.com/github.com/AvaloniaUI/Avalonia.Markup.Declarative/-/blob/src/Samples/AvaloniaImgTool/App.cs
+*/
