2f8a4efa9d8526b18427e64d9c074eab2ddde367
Nathaniel Collier <drxs3v3n@gmail.com>
2024-09-02 05:42:23 -0500

    Add simple open browser

---
+++ Test/WebBrowser/playwright/playwright_001HelloWorld/Program.cs
@@ -0,0 +1,38 @@
+﻿
+await useBrowser(async (browser,page) => {
+    await page.GotoAsync("https://news.ycombinator.com");
+    
+    Console.WriteLine("Press any key...");
+    Console.Read();
+});
+
+
+async Task useBrowser(Func<Microsoft.Playwright.IBrowser, Microsoft.Playwright.IPage, Task> onNewPageAvailable)
+{
+    using (var playwright = await Microsoft.Playwright.Playwright.CreateAsync())
+    {
+        await using (var browser = await playwright.Chromium.LaunchAsync(new()
+                        {
+                            Headless = false,
+                            // Can be "msedge", "chrome-beta", "msedge-beta", "msedge-dev", etc.
+                            // see: https://playwright.dev/dotnet/docs/browsers#chromium
+                            Channel = "chrome"
+                        }))
+        {
+            var context = await browser.NewContextAsync(options: new Microsoft.Playwright.BrowserNewContextOptions
+            {
+                AcceptDownloads = true
+            });
+            var page = await context.NewPageAsync();
+            try
+            {
+                await onNewPageAvailable(browser, page);
+            }
+            finally
+            {
+                await page.CloseAsync();
+                await browser.CloseAsync();
+            }
+        }
+    }
+}
