269b5630bd63b2bd0558eb594ed5495f4b069f75
Nathaniel A Collier <drxs3v3n@gmail.com>
2024-11-02 10:18:59 -0500

    Add an example on how to launch default web browser.   We are going to need this to do some login testing later

---
+++ Test/WebBrowser/OpenDefaultWebBrowser/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+
+OpenBrowser("https://www.google.com");
+
+
+System.Diagnostics.Process OpenBrowser(string url)
+{
+    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+    {
+        return Process.Start(new ProcessStartInfo("cmd", $"/c start {url}"));
+    }
+    
+    if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+    {
+        return Process.Start("xdg-open", url);
+    }
+    
+    if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+    {
+        return Process.Start("open", url);
+    }
+
+    throw new NotImplementedException(
+        $"Your browser is not implemented.   Your OS: [{RuntimeInformation.OSDescription}]");
+}
