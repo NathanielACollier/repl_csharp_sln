cc825fa20ad45b871ea35884d38324cdca57b48b
Nathaniel A Collier <drxs3v3n@gmail.com>
2024-11-14 18:49:55 -0600

    Prepare for experimenting with using sqlite in a blazor UI

---
+++ database/sqlite/sqliteEFCoreTest_BlazorDesktop/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+using Microsoft.Extensions.DependencyInjection;
+using Photino.Blazor;
+using sqliteEFCoreTest_BlazorDesktop;
+
+var appBuilder = PhotinoBlazorAppBuilder.CreateDefault(args);
+appBuilder.Services
+    .AddLogging();
+
+// register root component
+appBuilder.RootComponents.Add<App>("app");
+
+var app = appBuilder.Build();
+
+// customize window
+app.MainWindow
+    .SetTitle("SQLite EF Core Testing GUI");
+
+AppDomain.CurrentDomain.UnhandledException += (sender, error) =>
+{
+    app.MainWindow.ShowMessage("Fatal exception", error.ExceptionObject.ToString());
+};
+
+app.Run();
