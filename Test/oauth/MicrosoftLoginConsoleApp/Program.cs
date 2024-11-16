eae6c575e3e2a2dd4856fc14d8bae846e1d4c61d
Nathaniel A Collier <drxs3v3n@gmail.com>
2024-11-16 16:16:06 -0600

    Start working on the idea of what needs to be done to get Microsoft Login info

---
+++ Test/oauth/MicrosoftLoginConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿
+var log = new nac.Logging.Logger();
+nac.Logging.Appenders.ColoredConsole.Setup();
+
+try
+{
+    log.Info("Application started");
+    await run();
+}
+catch (Exception ex)
+{
+    log.Error($"General exception.  {ex}");
+}
+log.Info("Application finished");
+
+
+async Task run()
+{
+
+    var webServer = startWebServerToHandleOauthRedirect();
+}
+
+nac.WebServer.WebServerManager startWebServerToHandleOauthRedirect(){
+    var webServer = new nac.WebServer.WebServerManager();
+
+    webServer.OnNewRequest += (_s, args) =>
+    {
+
+    };
+
+    return webServer;
+}
+
