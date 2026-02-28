7bb816d0562547bb8d226626d799ec8f11fc6b4a
Nathaniel A Collier <drxs3v3n@gmail.com>
2026-02-28 15:57:09 -0600

    Replace the writing with a list, so it's the most efficient possible

--- algorithms/TestReadCharLengthAndModifyInFile/Program.cs
+++ algorithms/TestReadCharLengthAndModifyInFile/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using TestReadCharLengthAndModifyInFile.models;
 using repos = TestReadCharLengthAndModifyInFile.repos;
 
 Console.WriteLine("Application Started");
@@ -7,9 +8,29 @@ string targetFilePath = commonUtilitiesLib.File.getOutputPath("FilePositionReadW
 
 string product = repos.FileCharPositionRepo.Read(path: targetFilePath, startPos: 65, length: 10);
 
+/*
 repos.FileCharPositionRepo.Write(filePath: targetFilePath, startPosition: 65, length: 10, "Chicago Cubs");
 repos.FileCharPositionRepo.Write(filePath: targetFilePath, startPosition: 122, length: 2, "RE");
 repos.FileCharPositionRepo.Write(filePath: targetFilePath, startPosition: 124, length: 12, "Apple Sauce");
+*/
+
+repos.FileCharPositionRepo.Write(filePath: targetFilePath, replacementsInputArg: [
+    new SetPositionWithTextInfoModel
+    {
+        StartPosition = 65,Length = 10,
+        Text = "Little Rock"
+    },
+    new SetPositionWithTextInfoModel
+    {
+        StartPosition = 122, Length = 2,
+        Text = "RZ"
+    },
+    new SetPositionWithTextInfoModel
+    {
+        StartPosition = 124, Length = 12,
+        Text = "Peanut Butter Jam"
+    }
+]);
 
 Console.WriteLine("Application Done");
 
